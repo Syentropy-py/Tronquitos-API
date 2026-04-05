23519c2a63c0ee8873a356b6db137a5239efaad8
nikkaoyy <nico35134@gmail.com>
2026-04-04 21:42:48 -0500

    Push Inicial sin N8N (cambio en db de sqlite a PostgreSQL)

--- test_reservations.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-try:
-    with urllib.request.urlopen("http://localhost:5000/api/reservations") as res:
-        print("SUCCESS:", res.read().decode())
-except urllib.error.HTTPError as e:
-    print("HTTP ERROR:", e.code, e.read().decode())
